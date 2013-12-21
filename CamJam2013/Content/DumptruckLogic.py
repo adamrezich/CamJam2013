1744256cd728b21eace14f9b98de162973369f5e
Adam <adam@rezich.com>
2013-12-21 00:21:32 -0700

    added shitty sounds from mike

--- CamJam2013/Content/DumptruckLogic.py
+++ CamJam2013/Content/DumptruckLogic.py
@@ -10,7 +10,7 @@ class DumptruckLogic:
     Acceleration = 0.0005
     Deceleration = 0.00025
     Speed = 0
-    TurnSpeed = 0.02
+    TurnSpeed = 0.035
     Velocity = VectorMath.Vec3()
     Angle = 1.5
     
@@ -40,6 +40,9 @@ class DumptruckLogic:
             self.Angle += self.TurnSpeed
         if Zero.Keyboard.KeyIsDown(Keys.Right):
             self.Angle -= self.TurnSpeed
+        if Zero.Keyboard.KeyIsDown(Keys.Space):
+            dump = self.Space.CreateAtPosition("GenericSprite", self.Owner.Transform.Translation + VectorMath.Vec3(0, 0, -0.5))
+            dump.Sprite.SpriteSource = "citizen_dumped"
         while self.Angle >= math.pi * 2:
             self.Angle -= math.pi * 2
         while self.Angle < 0:
@@ -50,5 +53,6 @@ class DumptruckLogic:
         self.Owner.Transform.Translation += self.Velocity
         cam = self.Space.FindObjectByName("Camera")
         cam.Transform.Translation = self.Owner.Transform.Translation + VectorMath.Vec3(0, 0, 40)
+        cam.Camera.Size = 6 + abs(self.Speed) * 48
 
 Zero.RegisterComponent("DumptruckLogic", DumptruckLogic)
