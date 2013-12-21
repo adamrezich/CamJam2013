7fafaeaf150e9a80e9bf3d6617178bf5f40d5a29
Adam <adam@rezich.com>
2013-12-21 02:05:11 -0700

    made the game now a game

--- CamJam2013/Content/CitizenLogic.py
+++ CamJam2013/Content/CitizenLogic.py
@@ -2,12 +2,31 @@ import Zero
 import Events
 import Property
 import VectorMath
+import random
+import math
 
 class CitizenLogic:
     Dead = False
+    Timer = -1
+    Angle = 0
+    Speed = 0
     def Initialize(self, initializer):
         Zero.Connect(self.Owner, Events.CollisionStarted, self.OnCollisionStarted)
+        Zero.Connect(self.Space, Events.LogicUpdate, self.OnLogicUpdate)
     
+    def OnLogicUpdate(self, UpdateEvent):
+        if self.Dead:
+            return
+        self.Timer -= UpdateEvent.Dt
+        if self.Timer < 0:
+            self.Timer = 0.5 + random.random() * 4
+            self.Angle = random.random() * math.pi * 2
+            self.Speed = random.random() * 0.02
+        trans = self.Owner.Transform.Translation
+        trans += VectorMath.Vec3(math.cos(self.Angle), math.sin(self.Angle), 0) * self.Speed
+        self.Owner.Transform.Translation = trans
+        pass
+
     def OnCollisionStarted(self, CollisionEvent):
         pass
 
--- CamJam2013/Content/DumptruckLogic.py
+++ CamJam2013/Content/DumptruckLogic.py
@@ -7,18 +7,27 @@ import math
 import random
 
 class DumptruckLogic:
-    MaxSpeed = 0.1
+    MaxSpeed = 0.05
     Acceleration = 0.0005
     Deceleration = 0.00025
     Speed = 0
     TurnSpeed = 0.035
     Velocity = VectorMath.Vec3()
     Angle = 0
-    StartingUp = True
-    
+    StartedUp = False
+    BackingUp = False
+    DumpTimer = -1
+    MusicEmitter = None
+    EngineEmitter = None
+    StartTimer = 3
+        
     def Initialize(self, initializer):
         Zero.Connect(self.Space, Events.LogicUpdate, self.OnLogicUpdate)
         Zero.Connect(self.Owner, Events.CollisionStarted, self.OnCollisionStarted)
+        self.EngineEmitter = self.Space.CreateAtPosition("GenericSoundEmitter", VectorMath.Vec3())
+        self.MusicEmitter = self.Space.CreateAtPosition("GenericSoundEmitter", VectorMath.Vec3())
+        self.MusicEmitter.SoundEmitter.PlayCue("mus_01")
+        self.Owner.SoundEmitter.PlayCue("dt_start")
 
     def OnCollisionStarted(self, CollisionEvent):
         if CollisionEvent.OtherObject.Name == "Citizen":
@@ -28,9 +37,17 @@ class DumptruckLogic:
                 CollisionEvent.OtherObject.BoxCollider.Ghost = True
                 CollisionEvent.OtherObject.Transform.Translation -= VectorMath.Vec3(0, 0, 0.5)
                 CollisionEvent.OtherObject.SoundEmitter.PlayCue(random.choice(["c_die_01", "c_die_02", "c_die_03", "c_die_04", "c_die_05", "c_die_06"]))
+                self.Owner.SoundEmitter.PlayCue("dt_dirtdump")
         pass
 
-    def OnLogicUpdate(self, Event):
+    def OnLogicUpdate(self, UpdateEvent):
+        self.StartTimer -= UpdateEvent.Dt
+        if self.StartTimer < 0 and not self.StartedUp:
+            self.StartedUp = True
+            self.EngineEmitter.SoundEmitter.PlayCue("dt_engine_loop")
+        if not self.StartedUp:
+            return
+        self.DumpTimer -= UpdateEvent.Dt
         self.Owner.Transform.Rotation = VectorMath.Quat.EulerXYZ(0, 0, self.Angle)
         if Zero.Keyboard.KeyIsDown(Keys.Up):
             self.Speed = min(self.Speed + self.Acceleration, self.MaxSpeed)
@@ -46,8 +63,11 @@ class DumptruckLogic:
         if Zero.Keyboard.KeyIsDown(Keys.Right):
             self.Angle -= self.TurnSpeed
         if Zero.Keyboard.KeyIsDown(Keys.Space):
-            dump = self.Space.CreateAtPosition("GenericSprite", self.Owner.Transform.Translation + VectorMath.Vec3(0, 0, -0.5))
-            dump.Sprite.SpriteSource = "citizen_dumped"
+            if self.DumpTimer < 0:
+                self.DumpTimer = 0.15
+                dump = self.Space.CreateAtPosition("GenericSprite", self.Owner.Transform.Translation + VectorMath.Vec3(0, 0, -0.5))
+                dump.Sprite.SpriteSource = "citizen_dumped"
+                self.Owner.SoundEmitter.PlayCue("dt_dirtdump")
         while self.Angle >= math.pi * 2:
             self.Angle -= math.pi * 2
         while self.Angle < 0:
@@ -59,5 +79,15 @@ class DumptruckLogic:
         cam = self.Space.FindObjectByName("Camera")
         cam.Transform.Translation = self.Owner.Transform.Translation + VectorMath.Vec3(0, 0, 40)
         cam.Camera.Size = 6 + abs(self.Speed) * 48
+        if self.Speed < 0:
+            if not self.BackingUp:
+                self.Owner.SoundEmitter.PlayCue("dt_backupbeep")
+                self.BackingUp = True
+        else:
+            if self.BackingUp:
+                self.Owner.SoundEmitter.Stop()
+            self.BackingUp = False
+            
+            
 
 Zero.RegisterComponent("DumptruckLogic", DumptruckLogic)
